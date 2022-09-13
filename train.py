5b63285a14b130909beb186d985650cf9c58b217
siroshbashir <siroshbashir01@gmail.com>
2022-09-13 23:12:16 +0200

    check

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import precision_score
 from sklearn.metrics import accuracy_score
-from sagemaker.predictor import csv_serializer
 import os
 
 import boto3
